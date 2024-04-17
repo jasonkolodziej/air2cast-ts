d61a831cd816ea7f56d379daccbc69ac38d80406
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-04-16 19:41:01 -0500

    good

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,12 +4,31 @@ import * as fs from "fs";
 import type { PageData, PageServerLoad } from './$types';
 import { PWD } from "$env/static/private";
 
+interface Description {
 
+}
+interface Comment {
+    // _description: Array<String> | Description;
+    '$style'?: string;
+    _isCommented?: boolean;
+    _description: Array<String>;
+}
+
+interface Section<TName, String> {
+    _comments: Comment;
+    TName: KV;
+}
+
+interface KV {
+    _value: any;
+    '$type': String;
+    _description: Comment;
+}
 
 export const load: PageServerLoad = async ({ params, parent }) => { //? PageData    
     const layOutdata = await parent();
     console.info("PageServerLoad")
-    console.info(layOutdata)
+    console.info(layOutdata.data)
 	// return {
 	// 	post: await db.getPost(params.slug),
 	// };
