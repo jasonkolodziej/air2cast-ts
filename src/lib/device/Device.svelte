144458720d151f5e01b829a4e93ebe6236177d3c
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-04-17 01:34:23 -0500

    device

---
+++ src/lib/device/Device.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import { Button, ExpandableTile, ButtonSet,
+    Accordion, AccordionItem } from 'carbon-components-svelte';
+      import { CatalogPublish, Settings } from 'carbon-icons-svelte';
+      import type { KV } from '../../routes/+page.server';
+        export let items:Array<{title: string; description: string[]; children: Map<string, KV>}> = []
+  
+    let aopen = false;
+    let open = false;
+    let selected = 0;
+    //? Data Table
+    let expandedRowIds = [];
+    let selectedRowIds = [];
+    let filteredRowIds = [];
+    let shorterRows = [];
+    let pageSize = 5;
+    let page = 1;
+    $: {
+      console.log("expandedRowIds", expandedRowIds);
+      console.log("selectedRowIds", selectedRowIds);
+      console.log("filteredRowIds", filteredRowIds);
+    }
+  </script>
+
+<ExpandableTile tileExpandedLabel="View less" tileCollapsedLabel="View more">
+    <div slot="above">
+      <a
+        href="/"
+        on:click|preventDefault|stopPropagation={
+        () => console.log("Hello world")}>
+        <h4>Device Name</h4>
+        </a>
+      <br /><br />
+      <!-- <ButtonSet stacked> -->
+        <Button
+        icon={Settings}
+        iconDescription="Configure"
+        tooltipAlignment="start"
+        tooltipPosition="top"
+        size="small"
+        on:click={(e) => {
+          e.stopPropagation();
+          console.log("Hello world");
+        }}>
+      </Button>
+        <Button
+          kind="ghost"
+          icon={CatalogPublish}
+          size="small"
+          on:click={(e) => {
+            e.stopPropagation();
+            console.log("Hello world");
+          }}>
+          Logs
+        </Button>
+      <!-- </ButtonSet> -->
+      
+      <!-- <svelte:fragment slot="skip-to-content"> -->
+        <!-- <SkipToContent /> -->
+      <!-- </svelte:fragment> -->
+    </div>
+    <div slot="below">
+        <br/>
+        <!-- <Accordion align="start" slot="below">
+            <AccordionItem title="Natural Language Classifier" open={aopen}>
+              <p>
+                Natural Language Classifier uses advanced natural language processing and
+                machine learning techniques to create custom classification models. Users
+                train their data and the service predicts the appropriate category for the
+                inputted text.
+              </p>
+            </AccordionItem>
+          </Accordion> -->
+        Below the fold content here
+    </div>
+  </ExpandableTile>
+  
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import Configuration from "$lib/configuration/Configuration.svelte";
-	import { Form, Checkbox, Button } from "carbon-components-svelte";
+	import { Form, Checkbox, Button, Column, Row } from "carbon-components-svelte";
     import type { PageData } from './$types';
+	import Device from "$lib/device/Device.svelte";
   export let data: PageData;
 </script>
 
@@ -9,6 +10,16 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <!-- <Space/> -->
-<h2>Configure</h2>
+<h2>Devices</h2>
+<Row>
+  <Column>
+    <Device></Device>
+  </Column>
+  <Column><Device></Device></Column>
+  <Column>Column</Column>
+  <Column>Column</Column>
+</Row>
+
+<h3>Configure</h3>
 
 <Configuration items={data.pageData.data}/>
