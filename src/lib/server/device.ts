30aa041077ed9e5409bce4bc400e85d9a90ca604
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-05-01 15:24:46 -0500

    started client and reciever

--- src/lib/server/device.ts
+++ src/lib/server/device.ts
@@ -1,13 +1,17 @@
-import { type Listener, type Subscribable, AsyncEvent, Event } from "atvik";
+import { type AsyncSubscribable, type Listener, type Subscribable, AsyncEvent, Event } from "atvik";
 import type { MDNSService } from "tinkerhub-mdns";
 import { AbstractDestroyable } from "./service/type";
 import { Arp, ArpCall } from "./arp/arp";
 import { MAC, type Mac } from "./mac/MAC";
+import { PersistentClient, ReceiverController } from "@foxxmd/chromecast-client";
+import type { PersistentClientOptions } from "@foxxmd/chromecast-client/dist/cjs/src/persistentClient";
 
 interface DeviceServicePub extends Service {
     Record: MDNSService;
     MacAddress?: Mac;
-    readonly onDevice: Subscribable<this, [DeviceServicePub]>
+    Client: PersistentClient;
+    readonly onDevice: Subscribable<this, [DeviceServicePub]>;
+    readonly onClient: Subscribable<this, [PersistentClient]>;
 }
 
 class Device extends AbstractDestroyable implements DeviceServicePub {
@@ -17,7 +21,11 @@ class Device extends AbstractDestroyable implements DeviceServicePub {
     readonly id: string = 'device:service';
     Record: MDNSService;
     MacAddress?: Mac;
+    Client: PersistentClient;
+    Receiver?: ReceiverController.Receiver;
     protected readonly deviceEvent: Event<this, [DeviceServicePub]>;
+    protected readonly clientEvent: Event<this, [PersistentClient]>;
+    protected readonly receiverEvent: AsyncEvent<this, [ReceiverController.Receiver]>;
     protected readonly macEvent: Event<this, [Mac]> = new Event(this);
 
 
@@ -25,12 +33,26 @@ class Device extends AbstractDestroyable implements DeviceServicePub {
         super('device:service')
         this.Record = service;
         this.deviceEvent = new Event(this);
+        this.clientEvent = new Event(this);
+        this.receiverEvent = new AsyncEvent(this);
         this.deviceEvent.emit(this);
         // this.obtainMac().then(
         //     val => this.withUpdate(val)
 
         // )
+        const clientOptions = this.Address as PersistentClientOptions;
+        this.Client = new PersistentClient(clientOptions);
+        this.Client.connect().then(
+            () => {
+                this.Receiver = ReceiverController.createReceiver({client: this.Client});
+                this.receiverEvent.emit(this.Receiver);
+            }
+        );
         this.obtainMacAsync();
+    }
+
+    private get Address() {
+        return this.Record.addresses.at(0)!
     }
 
     private monitor(event: Event<this, [MDNSService, Mac?]>) {
@@ -44,11 +66,16 @@ class Device extends AbstractDestroyable implements DeviceServicePub {
         // event.emit(this);
     }
 
-    // get onDevice(): Subscribable<this, [MDNSService, Mac]> {
-    //     return this.deviceEvent.subscribable;
-    // }
     get onDevice(): Subscribable<this, [DeviceServicePub]> {
         return this.deviceEvent.subscribable;
+    }
+
+    get onClient(): Subscribable<this, [PersistentClient]> {
+        return this.clientEvent.subscribable;
+    }
+
+    get onReceiver(): AsyncSubscribable<this, [ReceiverController.Receiver]> {
+        return this.receiverEvent.subscribable;
     }
 
     get onMac(): Subscribable<this, [Mac]> {
@@ -62,9 +89,7 @@ class Device extends AbstractDestroyable implements DeviceServicePub {
             return;
         }
         const arp = new Arp(ArpCall.NAMED, someHost);
-        // let mac: MAC = (await availEvent.once().then(([calling, [id, val]]) => val.mac_address))
         const handle = arp.onAvailable((_, [id, arp]) => {
-            // console.log(arp);
             this.withMACUpdate(arp.mac_address);
         });
         // return mac;
@@ -91,20 +116,15 @@ class Device extends AbstractDestroyable implements DeviceServicePub {
 
     withUpdate(someSubscribable: MDNSService | MAC) {
         console.debug('DeviceService.onUpdate');
-        if(someSubscribable instanceof MAC) { // Mac
+        if(someSubscribable instanceof MAC) { // * Mac
         // if('_value' in someSubscribable) {
             console.debug('Mac update')
             this.MacAddress = someSubscribable;
             this.deviceEvent.emit(this);
-            // this.macEvent.emit(this.MacAddress);
-            // this.deviceEvent.asyncEmit(this);
-            // this.deviceEvent.emit(this.Record, this.MacAddress);
         }
-        if('name' in someSubscribable) { // MDNSService
+        if('name' in someSubscribable) { // * MDNSService
             this.Record = someSubscribable;
             this.deviceEvent.emit(this);
-            // this.deviceEvent.emit(this);
-            // this.deviceEvent.emit(this.Record, this.MacAddress!);
         }
     }
 
