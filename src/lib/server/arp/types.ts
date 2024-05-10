a12870e31c956fcd559e1fa55e70531e95d0c137
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-05-10 11:17:33 -0500

    arp has been moved

---
+++ src/lib/server/arp/types.ts
@@ -0,0 +1,39 @@
+import type { Subscribable } from "atvik";
+import type { ServicePublisher } from "tinkerhub-discovery";
+import type { MAC } from "../mac/MAC";
+
+
+export interface ArpDataEntry {
+    hostname: String;
+    ip_address: String;
+    mac_address: MAC;
+    interface_name: String;
+    scope: String | String[];
+    hw_type: String;
+};
+
+export interface ArpDataService extends ArpDataEntry {
+    readonly CallType?: ArpCall;
+    id: string;
+}
+  
+// export type ArpSubscriber = Subscribable<Arp, [EventCall, [ String, ArpDataEntry ]]>;
+  
+export interface ArpServicePublisher extends ServicePublisher {
+onEvent<T = EventCall>(calling:T): Subscribable<this, [typeof calling, [String, ArpDataEntry]]>;
+    readonly onEvents: Subscribable<this, [EventCall, [ String, ArpDataEntry ]]>;
+// readonly onUnavailable: Subscribable<this, [ ArpDataEntry ]>;
+}
+  
+export type ArpCallType = keyof typeof ArpCall;
+export enum ArpCall {
+    ALL = '-a',
+    NAMED = '-n'
+}
+export enum EventCall {
+    Available,
+    Unavailable,
+    Update,
+    Destroy
+}
+export type EventCallType = keyof typeof EventCall;
